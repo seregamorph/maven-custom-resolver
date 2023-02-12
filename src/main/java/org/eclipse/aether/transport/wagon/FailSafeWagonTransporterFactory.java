65b36aaa85b3b33a3f885c95894478317b77efd7
Sergey Chernov <sergey.ch@miro.com>
2023-02-12 14:50:35 +0100

    wip

---
+++ src/main/java/org/eclipse/aether/transport/wagon/FailSafeWagonTransporterFactory.java
@@ -0,0 +1,92 @@
+package org.eclipse.aether.transport.wagon;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.eclipse.aether.spi.connector.transport.Transporter;
+import org.eclipse.aether.spi.connector.transport.TransporterFactory;
+import org.eclipse.aether.spi.locator.Service;
+import org.eclipse.aether.spi.locator.ServiceLocator;
+import org.eclipse.aether.transfer.NoTransporterException;
+
+/**
+ *
+ */
+@Named("fail-safe-wagon")
+public final class FailSafeWagonTransporterFactory implements TransporterFactory, Service {
+
+    private WagonProvider wagonProvider;
+
+    private WagonConfigurator wagonConfigurator;
+
+    // it should be more than priority of WagonTransporterFactory (-1.0f)
+    private float priority = 1.0f;
+
+    /**
+     * Creates an (uninitialized) instance of this transporter factory. <em>Note:</em> In case of manual instantiation
+     * by clients, the new factory needs to be configured via its various mutators before first use or runtime errors
+     * will occur.
+     */
+    public FailSafeWagonTransporterFactory() {
+        // enables default constructor
+    }
+
+    @Inject
+    FailSafeWagonTransporterFactory(WagonProvider wagonProvider, WagonConfigurator wagonConfigurator) {
+        setWagonProvider(wagonProvider);
+        setWagonConfigurator(wagonConfigurator);
+    }
+
+    @Override
+    public void initService(ServiceLocator locator) {
+        setWagonProvider(locator.getService(WagonProvider.class));
+        setWagonConfigurator(locator.getService(WagonConfigurator.class));
+    }
+
+    /**
+     * Sets the wagon provider to use to acquire and release wagon instances.
+     *
+     * @param wagonProvider The wagon provider to use, may be {@code null}.
+     * @return This factory for chaining, never {@code null}.
+     */
+    public FailSafeWagonTransporterFactory setWagonProvider(WagonProvider wagonProvider) {
+        this.wagonProvider = wagonProvider;
+        return this;
+    }
+
+    /**
+     * Sets the wagon configurator to use to apply provider-specific configuration to wagon instances.
+     *
+     * @param wagonConfigurator The wagon configurator to use, may be {@code null}.
+     * @return This factory for chaining, never {@code null}.
+     */
+    public FailSafeWagonTransporterFactory setWagonConfigurator(WagonConfigurator wagonConfigurator) {
+        this.wagonConfigurator = wagonConfigurator;
+        return this;
+    }
+
+    @Override
+    public float getPriority() {
+        return priority;
+    }
+
+    /**
+     * Sets the priority of this component.
+     *
+     * @param priority The priority.
+     * @return This component for chaining, never {@code null}.
+     */
+    public FailSafeWagonTransporterFactory setPriority(float priority) {
+        this.priority = priority;
+        return this;
+    }
+
+    @Override
+    public Transporter newInstance(RepositorySystemSession session, RemoteRepository repository)
+            throws NoTransporterException {
+        return new WagonTransporter(wagonProvider, wagonConfigurator, repository, session);
+    }
+
+}
